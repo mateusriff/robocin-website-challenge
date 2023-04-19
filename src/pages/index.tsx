12f4c122f0d5cdac2c13afbd3fcf92aeb0664dd1
mateusriff <mateusnriff@gmail.com>
2023-04-19 16:30:42 -0300

    fix(Home): remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { Inter } from 'next/font/google'
 import { Banner, Activities, Sponsors, AboutUs } from '@/components'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
