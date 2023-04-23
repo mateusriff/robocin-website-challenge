894f694e4c5ccd3650e56c82f1c3cc54ba8799e2
mateusriff <mateusnriff@gmail.com>
2023-04-23 14:04:45 -0300

    fix(Activities): add page links

--- src/components/Activities/index.tsx
+++ src/components/Activities/index.tsx
@@ -4,15 +4,15 @@ import Card from "../Card";
 const Activities = () => {
     return(
         <CardsWrapper>
-            <Card title='Competições ->' href='#'>
+            <Card title='Competições ->' href='/categorias'>
                 Nós participamos de diversas competições, 
                 como a RoboCup, a IRONCup e a LARC.
             </Card>
-            <Card title='Pesquisa ->' href='#'>
-                Professores e estudantes pesquisando dentro 
+            <Card title='Pesquisa ->' href='/pesquisa'>
+                Nós pesquisamos soluções dentro 
                 dos problemas da robótica.
             </Card>
-            <Card title='Desenvolvimento ->' href='#'>
+            <Card title='Desenvolvimento ->' href='/pesquisa'>
                 Nós projetamos e desenvolvemos a eletrônica, 
                 a mecânica e a inteligência dos robôs.
             </Card>
