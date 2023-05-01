074ef6b29b098c48f35ead4a160abb1d5661cef1
mateusriff <mateusnriff@gmail.com>
2023-04-30 22:39:55 -0300

    fix(SocialMedia): add target prop to anchor tag

--- src/components/NavInterface/SocialMedia/index.tsx
+++ src/components/NavInterface/SocialMedia/index.tsx
@@ -5,7 +5,7 @@ const SocialMedia = () => {
     return (
         <SocialMediaWrapper>
             {socialMediaData.map((item, index) => (
-                <a key={index} href={item.url}>
+                <a key={index} href={item.url} target="_blank">
                     <StyledImage src={item.img} alt={item.alt} />
                 </a>
             ))}
